80677ab4af1122178373c85832d44ac69a7cd29a
Robert Wallis <smilingrob@gmail.com>
2018-03-07 00:34:47 -0800

    Mat4.transpose

--- src/Math/Matrix4.elm
+++ src/Math/Matrix4.elm
@@ -343,8 +343,8 @@ makeLookAt =
 index.
 -}
 transpose : Mat4 -> Mat4
-transpose =
-    Native.MJS.m4x4transpose
+transpose { m11, m21, m31, m41, m12, m22, m32, m42, m13, m23, m33, m43, m14, m24, m34, m44 } =
+    Mat4 m11 m12 m13 m14 m21 m22 m23 m24 m31 m32 m33 m34 m41 m42 m43 m44
 
 
 {-| Creates a transform from a basis consisting of 3 linearly independent vectors.
